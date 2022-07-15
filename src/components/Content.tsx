fb46766b30116471a92eec25d15e670e4ddbafbe
rvaz-dev <rvazn.ds@gmail.com>
2022-07-15 11:26:54 -0300

    desagio 2 terminado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,66 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import '../styles/content.scss';
+import { api } from '../services/api';
+import { MovieCard } from '../components/MovieCard';
+
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+
+
+export function Content({ selectedGenreId, selectedGenre }: ContentProps) {
+  // Criar um componente Content que recebe um array de objetos com as informações de cada filme.
+  // Cada filme deve ser renderizado com o componente MovieCard.
+  // O componente Content deve ser renderizado no lado direito da tela.
+  // O componente Content deve ser renderizado apenas quando o estado selectedGenreId for diferente de 0.
+  // O componente Content deve ser renderizado apenas quando o estado selectedGenre for diferente de {}.
+  // O componente Content deve ser renderizado apenas quando o estado selectedGenre for diferente de null.
+  // O componente Content deve ser renderizado apenas quando o estado selectedGenre for diferente de undefined.
+  // O componente Content deve ser renderizado apenas quando o estado selectedGenre for diferente de ''.
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`/movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </div>
+  );
+
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,51 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+
+import '../styles/sidebar.scss';
+import { api } from '../services/api';
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ContentProps {
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ selectedGenreId, handleClickButton }: ContentProps) {
+  // Criar um componente SideBar que recebe um array de objetos com as informações de cada gênero.
+  // Cada gênero deve ter um botão que, ao ser clicado, altera o valor do estado selectedGenreId para o id do gênero.
+  // O componente SideBar deve ser renderizado no lado esquerdo da tela.
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('/genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+  );
+
 }
